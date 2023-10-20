b9ab95ca26ba872e76196d31480422253a0e552f
ZHIBIN ZHOU <zhibinz2@uci.edu>
2023-10-20 16:27:43 -0700

    matfile

--- Examing_the_recording.m
+++ Examing_the_recording.m
@@ -1,6 +1,7 @@
 addpath(genpath('../TMSi_SAGA_matlab'))
 addpath(genpath('./util'))
 
+clear
 [EEGfileNameR]=uigetfile('*.Poly5');% select the Right player EEG
 Path_filenameR=[pwd '/' EEGfileNameR];
 [timeR,samplesR,TRIGGERindR,srR,channels_infoR,labelsR] = LoadTMSi(Path_filenameR);
